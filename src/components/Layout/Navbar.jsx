afd894e6fd2ad07538dc840ab31df9dfe9aca9e7
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-02-25 14:52:03 +0100

    added the side bar close to the nav

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import man from "/src/assets/image 1.png";
-import arrowdown from "/src/assets/FiChevronDown.svg";
+import ShowSidebar from "../Layout/Showsidebar";
+import man from "../../assets/image 1.png";
+import arrowdown from "../../assets/FiChevronDown.svg";
+
 const Navbar = ({ title }) => {
   return (
-    <div
-      className="flex px-4 md:px-[32px] py-[18px] justify-between h-[60px]"
-      style={{ background: "#fff" }}
-    >
+    <div className="flex items-center px-12  md:px-[32px] py-[18px] justify-between h-[60px] bg-white">
+      <ShowSidebar className="" />
       <p className="font-semibold tracking-wide ">{title}</p>
       <div className="flex items-center">
         <img src={man} alt="arrow" className="w-[24px] h-[24px] mr-3" />
