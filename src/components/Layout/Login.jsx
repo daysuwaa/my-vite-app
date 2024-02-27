b89a7705f8eaa834fd90c19bf96cc45b1d340361
Emmanuel Imolorhe <imolorhe@gmail.com>
2024-02-27 16:16:29 +0100

    update login ui

--- src/components/Layout/Login.jsx
+++ src/components/Layout/Login.jsx
@@ -51,38 +51,107 @@ const Login = () => {
   };
 
   return (
-    <div className="grid place-items-center h-screen bg-white">
-      <div className="text-white bg-[#22085C] rounded-lg max-w-96 w-full text-center p-8 shadow- lg">
-        <img
-          src={Logo}
-          alt="logo"
-          className="w-16 h-16 mx-auto mt-4 hue-rotate-60"
-        />
-        <h1 className="mt-2 mb-12 font-semibold">Log In</h1>
-        <form className="grid gap-6" onSubmit={handleLogin}>
-          <TextInput
-            placeholder="Email"
-            value={email}
-            setValue={setEmail}
-            iconJsx={<MdOutlineMail />}
-          />
-          <TextInput
-            type="password"
-            placeholder="Password"
-            value={password}
-            setValue={setPassword}
-            iconJsx={<RiLockPasswordLine />}
-          />
-          <button
-            type="submit"
-            className="bg-[#834CFC] text-white ml- 10 p-2 rounded font-semibold"
-          >
-            Login
-          </button>
-        </form>
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="#"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img className="w-8 h-8 mr-2" src={Logo} alt="logo" />
+          Chevron
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={(e) => e.preventDefault()}
+            >
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#834CFC] focus:border-[#834CFC] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="name@company.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#834CFC] focus:border-[#834CFC] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                  required=""
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-[#834CFC] dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-[#834CFC] dark:ring-offset-gray-800"
+                      required=""
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="remember"
+                      className="text-gray-500 dark:text-gray-300"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <a
+                  href="https://dashboard.squadco.com/reset-password-request"
+                  target="_blank"
+                  referrerPolicy="no-referrer"
+                  className="text-sm font-medium text-[#834CFC] hover:underline"
+                >
+                  Forgot password?
+                </a>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-[#834CFC] hover:bg-[#42257E] focus:ring-4 focus:outline-none focus:ring-[#834CFC] font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#834CFC] dark:hover:bg-[#42257E]"
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?{" "}
+                <a
+                  href="https://dashboard.squadco.com/sign-up"
+                  target="_blank"
+                  referrerPolicy="no-referrer"
+                  className="font-medium text-[#834CFC] hover:underline"
+                >
+                  Sign up
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
-      <Toaster />
-    </div>
+    </section>
   );
 };
 
